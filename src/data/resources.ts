f2428be25398762dc7d86a6e7f8cff4d93d05668
odia-vaishnava-songs <daitariswain7@gmail.com>
2026-04-22 14:31:39 +0530

    feat: add resources data module for odia songs collection

--- src/data/resources.ts
+++ src/data/resources.ts
@@ -480,7 +480,7 @@ export const RESOURCES: Resource[] = [
         type: 'html',
         author: 'ଶ୍ରୀଲ ଭକ୍ତି ବିନୋଦ ଠାକୁର',
         published: true,
-        status: ''
+        status: 'NOT_DONE'
     },
     {
         id: 'song-kijanikibale',
